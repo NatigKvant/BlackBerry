0b06695fafa8bafde55bcaab6aebc06067d638e4
Kvant <natikibishov@gmail.com>
2021-04-16 14:01:05 +0300

    added Preloader

--- src/Redux/usersReducer.js
+++ src/Redux/usersReducer.js
@@ -3,12 +3,14 @@ const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING'
 
 let initialState = {
   usersData:[ ],
   pageSize: 5,
   totalUsersCount: 0,
-  currentPage: 2
+  currentPage: 2,
+  isFetching: false
 }
 
 
@@ -40,10 +42,13 @@ const usersReducer = (state = initialState, action) => {
         }
         case SET_CURRENT_PAGE: {
           return { ...state, currentPage: action.currentPage }
-      }
-      case SET_TOTAL_USERS_COUNT: {
+        }
+        case SET_TOTAL_USERS_COUNT: {
         return { ...state, totalUsersCount: action.count }
-    }
+        }
+        case TOGGLE_IS_FETCHING: {
+          return { ...state, isFetching: action.isFetching }
+        }
   
    default: 
        return state;
@@ -60,5 +65,7 @@ export const setUsersAC = (users) => ({type: SET_USERS, users })
 export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage })
 
 export const setUsersTotalCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount })
+
+export const toggleIsFetchingAC = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching })
 
 export default usersReducer;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { followAC, setCurrentPageAC, setUsersTotalCountAC, setUsersAC, unfollowAC } from '../../Redux/usersReducer';
+import { followAC, setCurrentPageAC, setUsersTotalCountAC, setUsersAC, unfollowAC, toggleIsFetchingAC } from '../../Redux/usersReducer';
+import * as axios from 'axios';
 import Users from './Users';
+import Preloader from '../common/Preloader/Preloader';
+
+
+class UsersContainer extends React.Component {
+
+    
+  componentDidMount() {
+     this.props.toggleIsFetching(true)
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+      .then(response => {
+        this.props.toggleIsFetching(false)
+          this.props.setUsers(response.data.items);
+          this.props.setTotalUsersCount(response.data.totalCount);
+      });
+  }
+
+  onPageChanged = (pageNumber) => {
+      this.props.setCurrentPage(pageNumber);
+      this.props.toggleIsFetching(true)
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+      .then(response => {
+        this.props.toggleIsFetching(false)
+          this.props.setUsers(response.data.items);
+          
+      }); 
+  }
+
+
+render() {
+        return <>
+        { this.props.isFetching ? <Preloader /> : null}
+        <Users totalUsersCount = {this.props.totalUsersCount}
+                      pageSize = {this.props.pageSize}
+                      currentPage = {this.props.currentPage}
+                      onPageChanged = {this.onPageChanged}
+                      usersData = {this.props.usersData}
+                      follow = {this.props.follow}
+                      unfollow = {this.props.unfollow}
+         />
+         </>
+         }
+}
 
 
 let mapStateToProps = (state) => {
@@ -10,7 +53,8 @@ let mapStateToProps = (state) => {
   usersData: state.usersPage.usersData,
   pageSize: state.usersPage.pageSize,
   totalUsersCount: state.usersPage.totalUsersCount,
-  currentPage: state.usersPage.currentPage
+  currentPage: state.usersPage.currentPage,
+  isFetching: state.usersPage.isFetching
   }
 }
 
@@ -30,8 +74,11 @@ let mapDispatchToProps = (dispatch) => {
     },
     setTotalUsersCount: (totalCount) => {
       dispatch(setUsersTotalCountAC(totalCount))
+    },
+    toggleIsFetching:(isFetching) => {
+      dispatch(toggleIsFetchingAC(isFetching));
     }
   }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Users);
+export default connect(mapStateToProps,mapDispatchToProps)(UsersContainer);
