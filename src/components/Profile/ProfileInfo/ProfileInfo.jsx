8a3f7f2c69d8dd7bd2cb9e55f5ed36f21bce431e
Kvant <natikibishov@gmail.com>
2021-04-21 14:53:12 +0300

    Profile Navigation

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -13,7 +13,7 @@ const ProfileInfo = (props) => {
   return (
     <div>
       <div>
-        <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg' />
+        <img /* src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg' */ />
       </div>
       <div className = {s.descriptionBlock}>
         <img src ={props.profile.photos.large} />
